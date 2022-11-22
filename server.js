4a768efed1c99963f9c9ab1a347eb42440bddc67
Petr Broz <petr.broz@autodesk.com>
2022-11-22 14:44:14 +0100

    Added simple state sharing using socket.io.

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 const { PORT } = require('./config.js');
 
-let app = express();
+const app = express();
+const server = http.createServer(app);
+const io = new socketio.Server(server);
+
 app.use(express.static('wwwroot'));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/models.js'));
-app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('join-channel', function (data) {
+        socket.join(data.urn);
+    });
+    socket.on('leave-channel', function (data) {
+        socket.leave(data.urn);
+    });
+    socket.on('update-state', function (data) {
+        socket.to(data.urn).emit('state-changed', data.state);
+    });
+});
+
+server.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,9 +1,51 @@
 import { initViewer, loadModel } from './viewer.js';
 
+const socket = io();
+const presenter = new URLSearchParams(window.location.search).has('presenter');
+
 initViewer(document.getElementById('preview')).then(viewer => {
     const urn = window.location.hash?.substring(1);
     setupModelSelection(viewer, urn);
+    setupStateSharing(viewer);
 });
+
+function setupStateSharing(viewer) {
+    if (presenter) {
+        // If you're a presenter, send "update-state" message (max. once every 500ms)
+        // with the URN of the current model, and the viewer state
+        let lastUpdateTimestamp = Date.now();
+        function sendStateUpdate() {
+            if (Date.now() - lastUpdateTimestamp > 500) {
+                console.log('Sending state update...');
+                socket.emit('update-state', {
+                    urn: viewer.model.getData().urn,
+                    state: viewer.getState()
+                });
+                lastUpdateTimestamp = Date.now();
+            }
+        }
+        viewer.addEventListener(Autodesk.Viewing.MODEL_ROOT_LOADED_EVENT, sendStateUpdate);
+        viewer.addEventListener(Autodesk.Viewing.CAMERA_CHANGE_EVENT, sendStateUpdate);
+        viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, sendStateUpdate);
+    } else {
+        // If you're not a presenter, join a specific channel based on the URN
+        // of the currently loaded model, and listen to "state-changed" messages
+        let lastLoadedUrn = null;
+        viewer.addEventListener(Autodesk.Viewing.MODEL_ROOT_LOADED_EVENT, function () {
+            if (lastLoadedUrn) {
+                console.log('Leaving channel for model', lastLoadedUrn);
+                socket.emit('leave-channel', { urn: lastLoadedUrn });    
+            }
+            lastLoadedUrn = viewer.model.getData().urn;
+            console.log('Joining channel for model', lastLoadedUrn);
+            socket.emit('join-channel', { urn: lastLoadedUrn });
+        });
+        socket.on('state-changed', (state) => {
+            console.log('Received state update...');
+            viewer.restoreState(state, null, false);
+        });
+    }
+}
 
 async function setupModelSelection(viewer, selectedUrn) {
     const dropdown = document.getElementById('models');
